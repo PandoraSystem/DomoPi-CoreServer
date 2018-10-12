0b04259996de670033776faafcaf820aff1113e2
Marco Novelli <marconovelli.85@gmail.com>
2018-10-12 23:27:29 +0200

    nuovo progetto

---
+++ Dom/src/com/marktech/domotica/servertcp/Server.java
@@ -0,0 +1,100 @@
+package com.marktech.domotica.servertcp;
+
+/**
+ * Created by PC-Fisso on 12/10/2018.
+ */
+public class Server implements Runnable {
+
+    private Thread threadServer;
+    private boolean socketStop;
+    private int porta;
+
+    /**
+     *
+     * COSTRUTTORE
+     *
+     * Configuro il server con solo la porta
+     *
+     * @param porta
+     */
+    public Server(int porta) {
+        this.porta = porta;
+    }
+
+    /************************************
+     *
+     *
+     * PROPERTY GETTER AND SETTER
+     *
+     *
+     *************************************/
+
+    public boolean isSocketStop() {
+        return socketStop;
+    }
+
+    public void setSocketStop(boolean socketStop) {
+        this.socketStop = socketStop;
+    }
+
+    public int getPorta() {
+        return porta;
+    }
+
+    public void setPorta(int porta) {
+        this.porta = porta;
+    }
+
+
+
+
+    @Override
+    public void run() {
+
+        // Controllo di flusso per lasciare il processo attivo. Unico modo per killare il processo
+        while(!isSocketStop()){
+
+            //attendi una connessione
+            //ricava il messaggio in arrivo
+            //rispondi al messaggio
+        }
+
+    }
+
+
+
+    public boolean StartServer()
+    {
+        try {
+            threadServer = new Thread(this,"ThreadServer");
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }   return true;
+    }
+
+    public void StopServer()
+    {
+        setSocketStop(true);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+//Start
+
+//Stop
+
+//
