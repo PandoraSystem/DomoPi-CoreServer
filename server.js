4991f146c17de0bf352b6e01a62d9cd09c070caa
Denni Bevilacqua <dennib@live.it>
2018-10-15 21:53:26 +0200

    Correctly initialize and mantain TCP connection, passing dummy data through "/communicate" route

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 var express = require('express')
 var app = express()
 var routes = require('./webapp/routes/main')
+var net = require('net')
+
+// Create TCP Socket
+const socket = net.createConnection({
+	port:9000,
+	host:'localhost'
+}, () => {
+    // 'connect' listener
+    console.log('connected to server!')
+    socket.write('Hi from NodeJs main file!\r\n')
+})
+
+// Listen for incoming data and log it to console
+socket.on('data',
+	(data) => {
+        console.log(data.toString())
+	}
+)
+
+// Notifiy connection closing to console
+socket.on('end',
+	() => {
+    	console.log('disconnected from server')
+	}
+)
 
 // Retrieve App Routes
-routes(app)
+routes(app, socket)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
