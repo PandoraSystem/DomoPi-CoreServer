99f28902ba5d4d9de74a28c78c7658712f356344
Marco Novelli <marconovelli.85@gmail.com>
2018-11-14 00:12:59 +0100

    +Periferica di test creata +Aggiunta la serializzazione della periferica +Aggiunta la classe Interfaccia contenitore di periferiche

---
+++ Dom/src/com/marktech/domopi/controller/interfaccia/Interfaccia.java
@@ -0,0 +1,31 @@
+package com.marktech.domopi.controller.interfaccia;
+
+
+
+import com.marktech.domopi.controller.periferiche.Iperiferica;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Marco on 14/11/2018.
+ */
+public class Interfaccia {
+    Map<String,Iperiferica> mappaPeriferiche;
+
+    public Interfaccia() {
+        this.mappaPeriferiche = new HashMap<>();
+    }
+
+    void addPeriferica(Iperiferica iperiferica){
+        mappaPeriferiche.put(iperiferica.getId(),iperiferica);
+    }
+
+    void delPeriferica(Iperiferica iperiferica){
+        mappaPeriferiche.remove(iperiferica.getId());
+    }
+
+    void delPeriferica(String idPeriferica){
+        mappaPeriferiche.remove(idPeriferica);
+    }
+}
